e0ab4d9a22442cf4ef3be136cdc5d94287a18ea0
Cas <cas@vorteq.nl>
2019-12-09 17:54:24 +0100

    added 403 on options

--- api/src/controllers/beer.js
+++ api/src/controllers/beer.js
@@ -3,12 +3,15 @@ import Beer from "../models/beer";
 
 
 exports.options = function (req, res, next) {
-
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.send(200);
 
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        res.send(403);
+    }
 };
 
 // Create endpoint /api/beers for POST

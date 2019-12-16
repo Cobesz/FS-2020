c1784ea5e4a336612f1adec4a41d76254910f16f
Cas <cas@vorteq.nl>
2019-12-16 13:49:48 +0100

    changed wildcard to actual api url

--- api/src/main.js
+++ api/src/main.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Zorgt ervoor dat CORS en de bijhorende allows in orde zijn
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://145.24.222.116:8000/api/beers');
     res.setHeader('Access-Control-Allow-Headers', "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,DELETE,OPTIONS');
     next();

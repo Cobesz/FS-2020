4f8bb812ceb21f6491f7ddfbd92bc1d37238f075
Cas <cas@vorteq.nl>
2019-12-18 19:32:21 +0100

    moved detail up and added the query's back for pagination

--- api/src/router.js
+++ api/src/router.js
@@ -5,18 +5,18 @@ import * as authController from "./controllers/auth";
 
 const router = express.Router();
 
-// Create endpoint handlers for /beers
-router.route('/beers')
-    .get(beerController.getBeers)
-    .post(beerController.postBeers)
-    .options(beerController.optionsCollection);
-
 // Create endpoint handlers for /beers/:beer_id
 router.route('/beers/:beer_id')
     .get(beerController.getBeer)
     .put(beerController.putBeer)
     .delete(beerController.deleteBeer)
     .options(beerController.optionsDetail);
+
+// Create endpoint handlers for /beers
+router.route('/beers/:start?/:limit?')
+    .get(beerController.getBeers)
+    .post(beerController.postBeers)
+    .options(beerController.optionsCollection);
 
 // // Create endpoint handlers for /users
 // router.route('/users')
